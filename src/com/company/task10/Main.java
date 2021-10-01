ab5bed5a440a0ebe41ded1c2d6af5673535ec2ce
scrollz <kirill-dunaev2424@mail.ru>
2021-10-01 15:36:53 +0300

    1-7 full

--- src/com/company/task10/Main.java
+++ src/com/company/task10/Main.java
@@ -4,28 +4,81 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(ten(123456789));
-        twelve();
+        //System.out.println(ten(123456789));
+        //System.out.println(eleven());
+        //twelve();
+        //thirteen();
+        five(12345);
     }
 
-    public static int ten(int n) {
+    public static int one(int n) {
         if (n < 10)
             return n;
         else {
-            return n%10*(int)(Math.pow(10, (int)Math.log10(n))) + ten(n/10);
+            return n%10*(int)(Math.pow(10, (int)Math.log10(n))) + one(n/10);
         }
     }
 
-    public static void twelve(){
+    public static int two(){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        return switch (n) {
+            case 1 -> 1 + two();
+            case 0 -> twoHelp();
+            default -> two();
+        };
+    }
+
+    public static int twoHelp(){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        return switch (n) {
+            case 1 -> 1 + two();
+            case 0 -> 0;
+            default -> two();
+        };
+    }
+
+    public static void three(){
         Scanner in = new Scanner(System.in);
         int s = in.nextInt();
         if (s == 0) {
             return;
         }
-        else {
-            if (s%2 == 1)
-                System.out.println(s);
-            twelve();
+        else
+            if (s%2 == 1){
+                three();
+                System.out.print(s + " ");
+            }
+            else three();
+    }
+
+    public static void four() {
+        Scanner in = new Scanner(System.in);
+        int s = in.nextInt();
+        if (s == 0)
+            return;
+        fourHelp();
+        System.out.println(s);
+    }
+
+    public static void fourHelp(){
+        Scanner in = new Scanner(System.in);
+        int s = in.nextInt();
+        if (s ==0)
+            return;
+        four();
+    }
+
+    public static void five(int n){
+        if (n < 10){
+            System.out.print(n);
+        }
+        else{
+            System.out.print(n/(int)(Math.pow(10, (int)Math.log10(n))) + " ");
+            five(n%(int)(Math.pow(10, (int)Math.log10(n))));
         }
     }
+
+
 }
